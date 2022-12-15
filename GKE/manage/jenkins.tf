36e3c890b54c27da256173a0b9185ffe062d82aa
Calvin Li <calvin.tianli@peacehaven.co>
2022-12-15 14:09:31 -0500

    update helm provider for jenkins again

--- GKE/manage/jenkins.tf
+++ GKE/manage/jenkins.tf
@@ -1,6 +1,12 @@
 provider "helm" {
+#  tiller_image = "gcr.io/kubernetes-helm/tiller:${var.helm_version}"
+
   kubernetes {
-    config_path = "~/.kube/config"
+    host                   = "${data.google_container_cluster.my_cluster.endpoint}"
+    token                  = "${data.google_client_config.default.access_token}"
+    client_certificate     = "${base64decode(data.google_container_cluster.my_cluster.master_auth.0.client_certificate)}"
+    client_key             = "${base64decode(data.google_container_cluster.my_cluster.master_auth.0.client_key)}"
+    cluster_ca_certificate = "${base64decode(data.google_container_cluster.my_cluster.master_auth.0.cluster_ca_certificate)}"
   }
 }
 
