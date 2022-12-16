891a7c9a07e47f2ab8f349ce383c3c4aecb9af9c
Calvin Li <calvin.tianli@peacehaven.co>
2022-12-16 00:49:36 -0500

    remove nginx configuration and service. cleanup kubernetes.tf

--- GKE/manage/kubernetes.tf
+++ GKE/manage/kubernetes.tf
@@ -1,13 +1,3 @@
-/*
-data "terraform_remote_state" "gke" {
-  backend = "local"
-
-  config = {
-    path = "../provision/terraform.tfstate"
-  }
-}
-*/
-
 data "terraform_remote_state" "gke" {
   backend = "remote"
   config = {
@@ -39,25 +29,4 @@ provider "kubernetes" {
 
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
-}
-
-resource "kubernetes_service" "nginx" {
-  metadata {
-    name = "nginx-example"
-  }
-  spec {
-    selector = {
-      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
-    }
-    port {
-      port        = 80
-      target_port = 80
-    }
-
-    type = "LoadBalancer"
-  }
-}
-
-output "nginx_lb_ip" {
-  value = kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.ip
 }
