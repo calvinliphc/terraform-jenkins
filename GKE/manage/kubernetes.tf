82433773d8df11d22b6b26208a8326ececce8e24
Calvin Li <calvin.tianli@peacehaven.co>
2022-12-15 23:44:53 -0500

    expose nginx as a kubernetes service loadbalancer

--- GKE/manage/kubernetes.tf
+++ GKE/manage/kubernetes.tf
@@ -40,3 +40,24 @@ provider "kubernetes" {
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
 }
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-example"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+output "nginx_lb_ip" {
+  value = kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.ip
+}
