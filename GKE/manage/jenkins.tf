c1e96a249b06579c123527c6d19c89dcf8326b31
Calvin Li <calvin.tianli@peacehaven.co>
2022-12-15 23:32:41 -0500

    explicitly include the version 4.2.17 in jenkins helm_release resource

--- GKE/manage/jenkins.tf
+++ GKE/manage/jenkins.tf
@@ -2,6 +2,7 @@ resource "helm_release" "jenkins" {
   name       = "jenkins"
   repository = "https://charts.jenkins.io"
   chart      = "jenkins"
+  version    = "4.2.17"
   namespace  = "jenkins"
   timeout    = 600
   values = [
