199e0bdd025084404f49829743ec7fa392c7b1d6
Calvin Li <calvin.tianli@peacehaven.co>
2022-12-21 12:30:31 -0500

    add EKS management configuration

---
+++ EKS/manage/jenkins.tf
@@ -0,0 +1,26 @@
+resource "helm_release" "jenkins" {
+  name       = "jenkins"
+  repository = "https://charts.jenkins.io"
+  chart      = "jenkins"
+  version    = "4.2.17"
+  namespace  = "jenkins"
+  timeout    = 600
+  values = [
+    file("../../values.yaml"),
+  ]
+
+  depends_on = [
+    kubernetes_namespace.jenkins,
+  ]
+}
+
+resource "kubernetes_namespace" "jenkins" {
+  metadata {
+    name = "jenkins"
+
+    labels = {
+      name        = "jenkins"
+      description = "jenkins"
+    }
+  }
+}
---
+++ EKS/manage/kubernetes.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.20.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
+    }
+  }
+}
+
+data "terraform_remote_state" "eks" {
+  backend = "local"
+
+  config = {
+    path = "../provision/terraform.tfstate"
+  }
+}
+
+# Retrieve EKS cluster information
+provider "aws" {
+  region = data.terraform_remote_state.eks.outputs.region
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      data.aws_eks_cluster.cluster.name
+    ]
+  }
+}
